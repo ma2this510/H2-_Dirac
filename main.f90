76361fa1a7d03c19efb2181e775e09614a960f1e
Mathis PANET <mathis.panet@gmail.com>
2025-07-19 16:35:17 +0200

    eta_slp = 2e-2

--- main.f90
+++ main.f90
@@ -23,12 +23,12 @@ program main
    m = '1.0d0' ! mass of the electron
    C = '137.035999679d0' ! check CODATA 1986
    R = '1.0d0' ! distance between the two nuclei
-   ximax = '50.0d0' ! maximum position of the B-spline on z-axis
+   ximax = '30.0d0' ! maximum position of the B-spline on z-axis
    ximin = '1.0d0' ! minimum position of the B-spline on z-axis
    jz2 = one ! Quantum number will be divided by 2
    ! epsilon = '1.0d-5' ! machine epsilon
    epsilon = zero ! machine epsilon
-   eta_slp = '4.0d-2' ! paramet for the generation of the knot vector on eta
+   eta_slp = '2.0d-2' ! paramet for the generation of the knot vector on eta
    !-------------------------------------------------
    save_step = .false. ! Save matrices at each step
    !-------------------------------------------------
