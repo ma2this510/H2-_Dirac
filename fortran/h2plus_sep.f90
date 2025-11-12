c8cba56b3187e2b279b18590f19ca18a412538c4
Mathis PANET <mathis.panet@gmail.com>
2025-11-12 09:23:11 +0100

    add trim string file name

--- fortran/h2plus_sep.f90
+++ fortran/h2plus_sep.f90
@@ -942,7 +942,7 @@ contains
         print *, "Time taken to calculate C11one integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C11one_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C11one_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C11one(i, :), 10, 30, 10)
             end do
@@ -959,7 +959,7 @@ contains
         print *, "Time taken to calculate C11two integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C11two_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C11two_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C11two(i, :), 10, 30, 10)
             end do
@@ -976,7 +976,7 @@ contains
         print *, "Time taken to calculate C22one integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C22one_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C22one_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C22one(i, :), 10, 30, 10)
             end do
@@ -993,7 +993,7 @@ contains
         print *, "Time taken to calculate C22two integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C22two_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C22two_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C22two(i, :), 10, 30, 10)
             end do
@@ -1010,7 +1010,7 @@ contains
         print *, "Time taken to calculate C11three integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C11three_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C11three_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C11three(i, :), 10, 30, 10)
             end do
@@ -1027,7 +1027,7 @@ contains
         print *, "Time taken to calculate C22three integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C22three_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C22three_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C22three(i, :), 10, 30, 10)
             end do
@@ -1044,7 +1044,7 @@ contains
         print *, "Time taken to calculate C12three integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C12three_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C12three_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C12three(i, :), 10, 30, 10)
             end do
@@ -1061,7 +1061,7 @@ contains
         print *, "Time taken to calculate C21three integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/C21three_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/C21three_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(C21three(i, :), 10, 30, 10)
             end do
@@ -1078,7 +1078,7 @@ contains
         print *, "Time taken to calculate S11one integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/S11one_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/S11one_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(S11one(i, :), 10, 30, 10)
             end do
@@ -1095,7 +1095,7 @@ contains
         print *, "Time taken to calculate S22one integral: ", tm1 - tm0
 
         if (save_step) then
-            open (10, file = folder_name//"/S22one_sep.csv", status = 'replace')
+            open (10, file = trim(folder_name)//"/S22one_sep.csv", status = 'replace')
             do i = 1, n**2
                 call write_csv(S22one(i, :), 10, 30, 10)
             end do
@@ -1129,7 +1129,7 @@ contains
 
         if (save_step) then
             print *, "Saving C_mat matrix to file..."
-            open (unit = 13, file = folder_name//'/C_mat.csv', status = 'replace')
+            open (unit = 13, file = trim(folder_name)//'/C_mat.csv', status = 'replace')
             do i = 1, 4 * n**2
                 call write_csv(C_mat(i, :), 13, 50, 30)
             end do
@@ -1157,7 +1157,7 @@ contains
 
         if (save_step) then
             print *, "Saving S_mat matrix to file..."
-            open (unit = 14, file = folder_name//'/S_mat.csv', status = 'replace')
+            open (unit = 14, file = trim(folder_name)//'/S_mat.csv', status = 'replace')
             do i = 1, 4 * n**2
                 call write_csv(S_mat(i, :), 14, 50, 30)
             end do
@@ -1208,7 +1208,7 @@ contains
 
         print *, "Saving logs to file..."
         ! Save logs
-        open (unit = 1, file = folder_name//'/log_file', status = 'replace')
+        open (unit = 1, file = trim(folder_name)//'/log_file', status = 'replace')
         write (1, '(a, i4, a, i4, a, i4)') "Number of BSplines: ", n, " and Order of BSplines: ", d, " and Number of BSplines to remove: ", n_remove
         write (1, '(a)') "Speed of light: "
         call mpwrite(1, 35, 15, C)
@@ -1310,7 +1310,7 @@ contains
         print *, "Logs saved to log_file."
         print *, "Saving eigenvalues to file..."
 
-        open (unit = 12, file = folder_name//'/eigenvalues.txt', status = 'replace')
+        open (unit = 12, file = trim(folder_name)//'/eigenvalues.txt', status = 'replace')
         write (12, '(a, i4, a, i4, a, i4)') "Number of BSplines: ", n, " and Order of BSplines: ", d, " and Number of BSplines to remove: ", n_remove
         write (12, '(a)') "Speed of light: "
         call mpwrite(12, 35, 15, C)
