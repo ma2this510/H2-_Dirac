df8949f0c918907cf8cef3d4e716dd53a63fb758
Mathis PANET <mathis.panet@lkb.upmc.fr>
2025-11-17 13:39:19 +0100

    add knot_eta at 0

--- fortran/bspline_gen.f90
+++ fortran/bspline_gen.f90
@@ -283,12 +283,18 @@ contains
          result(itot) = -1*one
       end do
 
-      do i = 1, (n - d + 2*n_remove)/2
+      do i = 1, (n - d + 2*n_remove)/2 - 1
          itot = itot + 1
          result(itot) = -1*one + eta_slp*(one/eta_slp)**(((i - 1)*2*one)/((n - d - 1 + 2*n_remove)*one))
       end do
 
-      do i = (n - d + 2*n_remove)/2, 1, -1
+      itot = itot + 1
+      result(itot) = zero
+
+      itot = itot + 1
+      result(itot) = zero
+
+      do i = (n - d + 2*n_remove)/2 - 1, 1, -1
          itot = itot + 1
          result(itot) = one - eta_slp*(one/eta_slp)**(((i - 1)*2*one)/((n - d - 1 + 2*n_remove)*one))
       end do
