09dd50504b4919995f906c2fbf42f8b0d82cb656
Mathis Panet <mathis.panet@lkb.upmc.fr>
2025-11-28 18:32:29 +0100

    add importation of previous result

--- run_opti_nevergrad.py
+++ run_opti_nevergrad.py
@@ -5,12 +5,18 @@ import subprocess
 import re
 import time
 import threading
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import pandas as pd
+import os
 
 worker_counter = 0
 counter_lock = threading.Lock()
 
 thread_num = 24 
 max_run = 400
+
+comment_id = "Nevergrad optimization test n=18"
 
 print("Nevergrad version:", ng.__version__)
 print("Numpy version:", np.__version__)
@@ -19,7 +25,7 @@ def run_fun(xi_slp, eta_slp, xi_max):
 
     print(f"Running with parameters: xi_slp={xi_slp}, eta_slp={eta_slp}, xi_max={xi_max}")
 
-    command = f"python3 run_experiment.py with n=18 d=10 ximax={xi_max} eta_slp={eta_slp} xi_slp={xi_slp} -c 'Nevergrad optimization test n=18'"
+    command = f"python3 run_experiment.py with n=18 d=10 ximax={xi_max} eta_slp={eta_slp} xi_slp={xi_slp} -c '{comment_id}'"
 
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     print("Subprocess finished with return code:", result.returncode)
@@ -64,6 +70,53 @@ instrum = ng.p.Instrumentation(
 )
 
 optimizer = ng.optimizers.registry["NgIohTuned"](parametrization=instrum, budget=max_run, num_workers=thread_num)
+
+# -------------------------------------------------------------------------------------------------
+# Import already existing data from MongoDB
+load_dotenv()
+
+client = MongoClient(os.environ['URI'])
+db = client[os.environ['DB_NAME']]
+runs_collection = db['runs']
+
+runs = list(runs_collection.find({"meta.comment": comment_id}, {"config": 1, "result": 1}))
+if not runs:
+    print("No completed runs found in the given range.")
+    
+else:
+    df = pd.DataFrame(runs)
+    print(df.count())
+
+    df_conf = pd.json_normalize(df['config'])
+    df_conf.columns = [f'conf_{col}' for col in df_conf.columns]
+
+    df_res = pd.json_normalize(df['result'])
+    df_res.columns = [f'res_{col}' for col in df_res.columns]
+
+    col = df.columns.difference(['config','result'])
+    df_final_01 = pd.concat([df[col], df_conf, df_res], axis=1)
+
+    df_final_01['res_last_eigenvalue'] = df_final_01['res_last_eigenvalue'].astype(np.float64)
+
+    e_ref = -1.10264158103257716412
+
+    df_final_01 = df_final_01.sort_values(by=['_id']).reset_index(drop=True)
+
+    df_final_01['res_error'] = abs(df_final_01['res_last_eigenvalue'] - e_ref)
+    df_final_01['res_log_error'] = np.log10(df_final_01['res_error'])
+
+    for index, row in df_final_01.iterrows():
+        xi_slp = row['conf_xi_slp']
+        eta_slp = row['conf_eta_slp']
+        xi_max = row['conf_xi_max']
+        log_error = row['res_log_error']
+
+        candidate = optimizer.parametrization.new_child((xi_slp, eta_slp, xi_max))
+        optimizer.tell(candidate, log_error)
+        print(f"Imported run {index}: xi_slp={xi_slp}, eta_slp={eta_slp}, xi_max={xi_max}, log_error={log_error}")
+
+# -------------------------------------------------------------------------------------------------
+
 with futures.ThreadPoolExecutor(max_workers=optimizer.num_workers) as executor:
     recommendation = optimizer.minimize(delayed_run_fun, executor=executor, batch_mode=False)
 
