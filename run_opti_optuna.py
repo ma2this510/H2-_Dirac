f7a1cf6f9a23a657cd761e1e129786fb5662c5be
Mathis Panet <mathis.panet@lkb.upmc.fr>
2025-12-01 13:19:03 +0100

    increase delay and thread num for sam

--- run_opti_optuna.py
+++ run_opti_optuna.py
@@ -10,7 +10,7 @@ import time
 
 # load_dotenv()
 
-thread_num = 8 
+thread_num = 24 
 trial_num = 300
 n = 12
 
@@ -29,7 +29,7 @@ def run_fun_optuna(trial):
     with counter_lock:
         worker_id = worker_counter
         worker_counter += 1
-    delay = (worker_id % thread_num) * 1.2  # 1.2 sec delay between workers
+    delay = (worker_id % thread_num) * 1.4  # 1.4 sec delay between workers
     print(f"Worker {worker_id} starting after {delay:.1f}s delay")
     time.sleep(delay)
 
